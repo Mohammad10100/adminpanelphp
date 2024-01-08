513382d8843538c7a94e0c4f54d55849b1daeeca
Mohammad <mohammadabdullateef@eng.rizvi.edu.in>
2024-01-09 02:11:48 +0530

    feat: login and signup form html structure

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
+    <style>
+        <?php include './css/style.css' ?>
+    </style>
 </head>
+
 <body>
-    <?php
-    echo "PHP website"
-    ?>
+
+    <form id="signupForm" action="./auth/signup.php" method="POST">
+        <h2>Sign Up</h2>
+        <label for="signupUsername">Username:</label>
+        <input type="text" name="username" required>
+
+        <label for="signupPassword">Password:</label>
+        <input type="password" name="password" required>
+
+        <button type="submit">Sign Up</button>
+    </form>
+
+    <form id="loginForm" action="./auth/login.php" method="POST" style="margin-left: 20px;">
+        <h2>Login</h2>
+        <label for="loginUsername">Username:</label>
+        <input type="text"  name="username" required>
+
+        <label for="loginPassword">Password:</label>
+        <input type="password" name="password" required>
+
+        <button type="submit">Login</button>
+    </form>
+
 </body>
+
 </html>
